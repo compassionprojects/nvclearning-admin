a1b7dadc976ad3dc982d886d85b6fef976b4f81e
Madhusudhan Srinivasa <me@madhums.me>
2020-07-05 21:58:27 +0200

    lint and format code

--- src/migrations/20200705163421_add_authToken_to_user.js
+++ src/migrations/20200705163421_add_authToken_to_user.js
@@ -1,11 +1,11 @@
-exports.up = function(knex) {
-  return knex.schema.table('User', function(table) {
+exports.up = function (knex) {
+  return knex.schema.table('User', function (table) {
     table.text('authToken');
   });
 };
 
-exports.down = function(knex) {
-  knex.schema.table('User', function(table) {
+exports.down = function (knex) {
+  knex.schema.table('User', function (table) {
     table.dropColumn('authToken');
   });
 };
--- src/seeds/dev/initial-user.js
+++ src/seeds/dev/initial-user.js
@@ -2,13 +2,14 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync(process.env.ADMIN_PASSWORD, salt);
-const createdAt_utc = (new Date()).toISOString();
+const createdAt_utc = new Date().toISOString();
 const createdAt_offset = '+00:00';
 const authToken = crypto.randomBytes(32).toString('hex');
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('User').del()
+  return knex('User')
+    .del()
     .then(function () {
       // Inserts seed entries
       return knex('User').insert([
@@ -21,7 +22,7 @@ exports.seed = function(knex) {
           createdAt_offset,
           updatedAt_utc: createdAt_utc,
           updatedAt_offset: createdAt_offset,
-          authToken
+          authToken,
         },
       ]);
     });
