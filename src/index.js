78af109569810c5e01f9f453d39f04d776173b2a
Madhusudhan Srinivasa <me@madhums.me>
2020-07-05 16:27:45 +0200

    add blank keystone app

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+
+const PROJECT_NAME = 'vic';
+const knexOptions = require('./knexfile');
+const adapterConfig = { knexOptions };
+
+/**
+ * You've got a new KeystoneJS Project! Things you might want to do next:
+ * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
+ * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
+ */
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(adapterConfig),
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ enableDefaultRoute: true })],
+};
