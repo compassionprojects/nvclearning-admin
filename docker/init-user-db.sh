2bc96a2a8e5c4775c94c78f390fd00c7ae723da5
madhums <me@madhums.me>
2024-11-25 23:49:36 +0100

    update docker db settings

--- docker/init-user-db.sh
+++ docker/init-user-db.sh
@@ -3,6 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER admin;
-    CREATE DATABASE events-admin;
-    GRANT ALL PRIVILEGES ON DATABASE events-admin TO admin;
+    CREATE DATABASE cp-events-admin;
+    GRANT ALL PRIVILEGES ON DATABASE cp-events-admin TO admin;
 EOSQL
